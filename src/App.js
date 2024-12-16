dbc8b23fa35318977abda825d80883b7f4ecf677
insomnic123 <qaziayn@gmail.com>
2024-12-16 15:10:31 -0500

    amazing first steps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+import sadman from './sadman.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sadman} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ci3.googleusercontent.com/mail-img-att/AGAZnRpMcM3VT7xyWd9-caChYIHcBp8zyNBpsVetpPUNZwuz47DEp9-2k5DrwMpdeELyXp1DN2sWStocwgXOPcm_AqIYkyVvEaMRVS0V9FmpaAXeQTba1ywTrX_Oxv6v4gXeqfcjDPptLu2H9jyE_mTyNO1JI08sD-DyKApa-z-RzrvSJpKF8-DkyI-QceVGBUkqneEGu5PJ2431xHwSojXeqmzAPykeLGUWJWohAzTEWhc2vz-d-5jaN4mys3HOTCB7IgEUCIBGbjSgQv-GL1zvUmiH4OEE6C3R_LyacWWw9Mw9l3JiDXKzxAubatvmvb0YZcD97J8SmXuPrF0FsbNmVUXzqAR_GZ6VN3WsYibf-SpSxIyz0jjxwOTwFhRuI3WdjdE0t70oG_39EIyibaVxUOEUwzgN3aHMuoN02eZbSKy7SFSg_WdPW51rB5s9ENAFVVzmaFk9z8DezrcMwb7dJGl3sDjvy2P4pqPfDZr65WizMen9OH7tac8A0sVwhHSYNxUWOyBYqADqbw_wqkvpP0hBEh8u4CvW7sMhKpf_L1lR7ATW2pI51PgbLzJaO7YaeLzJk1s2k7IXoN3i2yHk5r4az3k75j2YymRZH_2F3UKkgyH0dZFQX3xOoNlgC4fh-uK7BHV03aKJPp4wKSsKEzyFuM3TjQHAmrtpKnagqTGnu64SQnA4MduaWdVn4VvNYu11YaokTblsbUdSErzCYb3JwL-JlGk3NSP-fBK_t2x9Ikc52Hp05r1lozbMNeRMiP7O--MSUMnrK_A26usw0vlSVY-8aq98H_vejBh_Y2Hr-7V-xTQhBJYNuiVIsYF0Q-m32NdL2qq914JVD2JIXRCCXOcVyvL6FCEr1MmkunnXYVfBpwFsvihmZ-GPz-R7hWP7TBM-YC8-qgR0rOLcgsf6ZdE1baDCrRdTzJbzvjGkM4bK1oU67_LQ76EAMqZnPWZhO1H_H4p9R0Ntyudtcmw59IL1vYXwr3VSnU19IzxxIfyUIrfFD5Pch_cE9Sw4vWj-5h4poLcNH1iNhhLfEPI=s0-l75-ft"
           target="_blank"
           rel="noopener noreferrer"
         >
